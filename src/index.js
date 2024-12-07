1c5d05e687e9af49d76cbc7b8f227c44f61c67b3
Сергей Баев <sergeybaev@MacBook-Pro-Sergej.local>
2024-12-07 17:11:16 +0300

    added validation of data but i need to update this in the future

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import  'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as yup from 'yup';
 
-console.log('Hello, world!');
-
 const shema = yup.string().url().required();
-
 const feeds = new Set();
 
-const form = document.getElementsByClassName('form');
+const form = document.getElementById('form');
 const rssInput = document.querySelector('.form-control');
 const rssContent = document.querySelector('#rss-content')
 
@@ -19,20 +16,21 @@ form.addEventListener('submit', (e) => {
   shema.validate(rssUrl)
     .then(() => {
       if (feeds.has(rssUrl)) {
-        rssInput.classList.add('error');
+        rssInput.classList.add('is-invalid');
         throw new Error('RSS поток уже добавлен');
       }
       return Promise.resolve();
     })
     .then(() => {
+      rssInput.classList.remove('is-invalid');
       feeds.add(rssUrl);
       rssContent.innerHTML += `<p>RSS поток: ${rssUrl} </p>`;
       rssInput.value = '';
       rssInput.focus();
     })
     .catch((error) => {
+      rssInput.classList.add('is-invalid');
       console.log('Validate error:', error);
-      rssInput.classList.add('error');
-      alert(error.message); 
+      alert(error.message);
     });    
 });
